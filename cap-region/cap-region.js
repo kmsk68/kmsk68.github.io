7f87e9dc52c7c54e8509c117b3b3ee6b981a7d11
kmsk68 <40442987+kmsk68@users.noreply.github.com>
2020-06-23 15:20:43 -0400

    upload cap-region.js

---
+++ cap-region/cap-region.js
@@ -0,0 +1,9 @@
+// JavaScript Documentfunction 
+myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
