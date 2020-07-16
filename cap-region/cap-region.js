270404dfea0128f18862d0725278d0d272583c6e
kmsk68 <40442987+kmsk68@users.noreply.github.com>
2020-07-16 13:50:05 -0400

    update cap-region.js

--- cap-region/cap-region.js
+++ cap-region/cap-region.js
@@ -1,9 +1,7 @@
 // JavaScript Documentfunction 
-myFunction() {
-  var x = document.getElementById("nav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+	navUL.classList.toggle('show');
+});
